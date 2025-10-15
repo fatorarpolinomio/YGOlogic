d1da3a0e48e76b0a2e2aa83e166bd37158770b9c
willianjsf <167502619+willianjsf@users.noreply.github.com>
2025-10-14 23:54:05 -0300

    Update YGOlogic.py

--- YGOlogic.py
+++ YGOlogic.py
@@ -63,12 +63,29 @@ class Player():
     def create_standard_deck():
     """cria e retorna uma lista de cartas padrão para um novo jogador"""
     deck = [
+        # Monsters ---> Percebi que precisamos colocar o nível das cartas de monstros
         Card("Mago Negro", 2500, 2100, CardType.MONSTER),
         Card("Dragão Negro de Olhos Vermelhos", 2400, 2000, CardType.MONSTER),
         Card("Dragão Branco de Olhos Azuis", 3000, 2500, CardType.MONSTER),
+        Card("Dragão Filhote", 1200, 700, CardType.MONSTER), # nível 3 (arrumar dps)
+        Card("Guardião Celta", 1400, 1200, CardType.MONSTER), # nível 4
+        Card("Dragão do Brilho", 1900, 1600, CardType.MONSTER), # nível 4
+        Card("Elfa Mística", 800, 2000, CardType.MONSTER), # nível 4
+        Card("Kaiser Violento", 1800, 1600, CardType.MONSTER), # nível 5
+        Card("Caveira Invocada", 2500, 1200, CardType.MONSTER), #nível 6
+        
+        # Magic
         Card("Raigeki", None, None, CardType.MAGIC, "Destrói todos os monstros no campo do oponente."),
-        Card("Monster Reborn", None, None, CardType.MAGIC, "Ressuscita um monstro de qualquer cemitério.")
-       
-        # temos que decidir quais cartas por no deck inteiro e será o mesmo para ambos jogadores?
+        Card("Reviver Monstro", None, None, CardType.MAGIC, "Ressuscita um monstro de qualquer cemitério."),
+        Card("Pote da Ganância", None, None, CardType.MAGIC, "Compre duas cartas."),
+        Card("Tufão Espacial Místico", None, None, CardType.MAGIC, "Escolha 1 Magia/Armadilha no campo; destrua o alvo."),
+        Card("Tempestade Pesada", None, None, CardType.MAGIC, "Destrua todos os Cards de Magia e Armadilha no campo.."),
+        
+        # Traps
+        Card("Força do Espelho", None, None, CardType.TRAP, "Quando um monstro do oponente declarar um ataque: destrua todos os monstros em Posição de Ataque que seu oponente controla."),
+        Card("Cilindro Mágico", None, None, CardType.TRAP, "Quando um monstro do oponente declarar um ataque: escolha o monstro atacante; negue o ataque e cause dano ao seu oponente igual ao ATK dele."),
+        Card("Buraco Armadilha", None, None, CardType.TRAP, "Quando seu oponente Invocar por Invocação-Normal ou Virar um monstro com 1000 ou mais de ATK: escolha o monstro; destrua o alvo."),
+        Card("Aparelho de Evacuação Obrigatória", None, None, CardType.TRAP, "Escolha 1 monstro no campo; devolva o alvo para a mão."),
+        Card("Negativação de Ataque", None, None, CardType.TRAP, "Quando um monstro do oponente declarar um ataque: escolha o monstro atacante; negue o ataque."),
     ]
     return deck
